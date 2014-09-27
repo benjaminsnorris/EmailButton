288f94ca20ad6d4fb14becdc8a431fb8373d9c0f
Ben Norris <ben@bsn.io>
2014-09-27 09:40:47 -0600

    Set up MFMailComposeViewController, although body and to fields are not set

--- EmailButton/EBMailButtonViewController.m
+++ EmailButton/EBMailButtonViewController.m
@@ -7,17 +7,32 @@
 //
 
 #import "EBMailButtonViewController.h"
+#import <MessageUI/MessageUI.h>
 
-@interface EBMailButtonViewController ()
+@interface EBMailButtonViewController () <MFMailComposeViewControllerDelegate>
 
 @end
 
 @implementation EBMailButtonViewController
 
 - (IBAction)sendEmail:(id)sender {
-
+    MFMailComposeViewController *mailViewController = [MFMailComposeViewController new];
+    mailViewController.mailComposeDelegate = self;
+    [mailViewController setSubject:@"My great feedback"];
+    [mailViewController setToRecipients:@[@"ben@bsn.io"]];
+    [mailViewController setMessageBody:@"I love this app so much! This is why:\n" isHTML:NO];
     
+    if ([MFMailComposeViewController canSendMail]) {
+        [self presentViewController:mailViewController animated:YES completion:^{
+            // Code that would run after the view is presented
+        }];
+    }
     
 }
+
+- (void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error {
+    [controller dismissViewControllerAnimated:YES completion:nil];
+}
+
 
 @end
